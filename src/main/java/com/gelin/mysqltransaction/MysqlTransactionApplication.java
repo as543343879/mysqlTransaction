6eaf78d99bfd8c20319a85b756704d66ef14dfa4
xiexiaoping <543343879@qq.com>
2023-01-28 21:50:58 +0800

    feat: 悬挂事务测试

---
+++ src/main/java/com/gelin/mysqltransaction/MysqlTransactionApplication.java
@@ -0,0 +1,13 @@
+package com.gelin.mysqltransaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MysqlTransactionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MysqlTransactionApplication.class, args);
+	}
+
+}
